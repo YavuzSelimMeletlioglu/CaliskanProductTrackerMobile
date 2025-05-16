19494d45f78aadb1d5cab33160aeaea3acdb475d
Yavuz Meletlioğlu <yselimmeletlioglu@icloud.com>
2025-05-16 18:48:51 +0300

    Atanan görev sayfası güncellendi.

--- src/pages/AssignedJobs.tsx
+++ src/pages/AssignedJobs.tsx
@@ -8,11 +8,12 @@ import {
   TouchableOpacity,
 } from "react-native";
 import React, { useEffect, useState } from "react";
-import { get } from "../api/api";
+import { deleteRequest, get, post } from "../api/api";
 import { Assignment } from "../types/types";
 import { Appbar } from "react-native-paper";
 import { dateConfig, logout } from "../functions";
 import { AddProcessorAssignment } from "../modals/AddProcessOrAssignment";
+import AntDesign from "@expo/vector-icons/AntDesign";
 
 type Props = {
   user_id?: string;
@@ -33,7 +34,6 @@ const AssignedJobs = ({ user_id }: Props) => {
     setIsRefreshing(true);
     const response = await get(`assignments/${user_id ?? ""}`);
     if (response && response.success) {
-      console.log(response.data);
       const modifiedData: Assignment[] = response.data.map(
         (item: Assignment) => {
           return {
@@ -55,42 +55,76 @@ const AssignedJobs = ({ user_id }: Props) => {
 
   const renderItem = ({ item }: { item: Assignment }) => {
     return (
-      <TouchableOpacity
-        style={styles.contentContainer}
-        onPress={() => {
-          setSelectedCompanyId(item.company_id);
-          setSelectedProductId(item.product_id);
-          setIsVisible(true);
-        }}>
-        <AddProcessorAssignment
-          visible={visible}
-          onDismiss={() => setIsVisible(false)}
-          company_id={selectedCompanyId ?? 1}
-          product_id={selectedProductId ?? 1}
-          assignment_id={item.assignment_id}
-          isAdmin={false}
-          fromAssignments={true}
-        />
-        <View
-          style={{
-            borderRadius: 10,
-            width: "100%",
+      <View
+        style={{ flexDirection: "row", alignItems: "center", columnGap: 5 }}>
+        <TouchableOpacity
+          style={styles.contentContainer}
+          onPress={() => {
+            setSelectedCompanyId(item.company_id);
+            setSelectedProductId(item.product_id);
+            setIsVisible(true);
+          }}>
+          <AddProcessorAssignment
+            visible={visible}
+            onDismiss={() => setIsVisible(false)}
+            company_id={selectedCompanyId ?? 1}
+            product_id={selectedProductId ?? 1}
+            assignment_id={item.assignment_id}
+            isAdmin={false}
+            fromAssignments={true}
+          />
+          <View style={styles.rowContainer}>
+            <View
+              style={{
+                height: "100%",
+                width: 10,
+                borderRadius: 10,
+                backgroundColor:
+                  item.completed_quantity === 0
+                    ? "red"
+                    : item.completed_quantity === item.quantity
+                    ? "green"
+                    : "orange",
+              }}
+            />
+            <View>
+              <View style={styles.rowContainer}>
+                <Text style={styles.itemText}>{item.company_name}</Text>
+                <Text style={styles.itemText}>{item.product_name}</Text>
+                <Text style={styles.itemText}>
+                  {item.last_date_completion.toLocaleDateString(
+                    "tr",
+                    dateConfig
+                  )}
+                </Text>
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-evenly",
+                  flex: 1,
+                }}>
+                <Text style={styles.itemText}>Toplam: {item.quantity}</Text>
+                <Text style={styles.itemText}>
+                  Kalan: {item.quantity - item.completed_quantity}
+                </Text>
+              </View>
+            </View>
+          </View>
+        </TouchableOpacity>
+        <AntDesign
+          name="delete"
+          size={24}
+          color="red"
+          onPress={() => {
+            deleteRequest("assignments", { assignment_id: item.assignment_id });
+            const newList = assignedJobs.filter(
+              (value) => item.assignment_id !== value.assignment_id
+            );
+            setAssignedJobs(newList);
           }}
         />
-        <View style={styles.rowContainer}>
-          <Text style={styles.itemText}>{item.company_name}</Text>
-          <Text style={styles.itemText}>{item.product_name}</Text>
-          <Text style={styles.itemText}>{item.quantity} adet</Text>
-        </View>
-        <View style={styles.rowContainer}>
-          <Text style={styles.itemText}>
-            Tamamlanan: {item.completed_quantity}
-          </Text>
-          <Text style={styles.itemText}>
-            {item.last_date_completion.toLocaleDateString("tr", dateConfig)}
-          </Text>
-        </View>
-      </TouchableOpacity>
+      </View>
     );
   };
 
@@ -142,7 +176,7 @@ const styles = StyleSheet.create({
   rowContainer: {
     width: "100%",
     alignItems: "center",
-    columnGap: 10,
+    columnGap: 5,
     flexDirection: "row",
     justifyContent: "space-between",
   },
@@ -150,6 +184,5 @@ const styles = StyleSheet.create({
     color: "#000",
     fontSize: 20,
     fontWeight: "bold",
-    flex: 1,
   },
 });
